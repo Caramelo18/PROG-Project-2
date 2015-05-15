00bd6720f0f3dcea14397954c511684f5851a036
Bruno Barros <brunocostabarros@hotmail.com>
2015-05-15 12:57:17 +0100

    Players

--- Proj 2/PLAYER.cpp
+++ Proj 2/PLAYER.cpp
@@ -1,29 +1,42 @@
 #include "BOMB.h"
 #include "PLAYER.h"
 #include "BOARD.h"
-
-
-using namespace std;
-
+#include <iostream>
 
 Player::Player(string playerName, string boardFilename)
 {
-
+	name = playerName;
+	board = Board::Board(boardFilename);
 }
 
 void Player::showBoard() const // shows the player’s board
 {
-
+	board.display();
 }
 
-//Bomb Player::getBomb() const // asks bomb target coordinates and creates the bomb
-//{
-//
-//}
+Bomb Player::getBomb() const // asks bomb target coordinates and creates the bomb
+{
+	string coordenates;
+	PositionChar char_coordenates;
+
+	std::cout << "Introduce your bomb coordenates (lin, col): ";
+	std::cin >> coordenates;
+
+	if (coordenates.size() != 2)
+		std::cout << "Invalid input! \n";
+	else
+	{
+		char_coordenates.lin = coordenates[0];
+		char_coordenates.col = coordenates[1];
+	}
+
+	Bomb::Bomb(char_coordenates);
+}
 
 void Player::attackBoard(const Bomb &b) // "receives" a bomb from the opponent;
 {
-
+	board.attack(b);
 }
+
 // updates own board taking into account the damages
 // caused by the bomb; BEFORE THAT… moves the ships
