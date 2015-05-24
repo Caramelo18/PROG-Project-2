c5c19cc4a297d69456db76f742bae3ec79a07b30
Fábio Caramelo <fabiocaramelo18@gmail.com>
2015-05-24 02:08:13 +0100

    scores continuados

--- Proj 2/main.cpp
+++ Proj 2/main.cpp
@@ -7,7 +7,7 @@
 #include <windows.h>
 #include <sstream>
 #include <stdlib.h>
-//#include "BOMB.h"
+#include "BOMB.h"
 #include "BOARD.h"
 #include "SHIP.h"
 #include "PLAYER.h"
@@ -19,14 +19,29 @@ void readScores(vector<Score> &scores)
 	fstream file("Highscores.txt");
 	string line, temp, name;
 	int score;
-	if (file.eof())
+	getline(file, line);
+	if (line == "") // se o ficheiro estiver vazio vai preencher o vector de scores com scores irrealistas
 	{
-		file << "Battleship Highscores" << endl;
-		for (int i = 0; i < 10; i++)
-			file << i + 1 << "  -  Player  -  999999" << endl;
-		file.close();
-		file.open("Highscores.txt");
+		for (unsigned int i = 0; i < scores.size(); i++)
+		{
+			scores[i].name = "Player";
+			scores[i].score = 999999;
+		}
+		/*file << "Battleship Highscores" << endl;
+		for (int i = 0; i < 10; i++) 
+			file << i + 1 << " - Player - 999999" << endl;
 		getline(file, line);
+		for (int i = 0; i < 10; i++)
+		{
+			getline(file, line);
+			stringstream ss(line);
+			ss >> temp >> temp >> name >> temp >> score;
+			scores[i].name = name;
+			scores[i].score = score;
+		}*/
+	}
+	else
+	{
 		for (int i = 0; i < 10; i++)
 		{
 			getline(file, line);
@@ -36,57 +51,64 @@ void readScores(vector<Score> &scores)
 			scores[i].score = score;
 		}
 	}
-	else
-	{
-		getline(file, line);
-		for (int i = 0; i < 10; i++)
-		{
-			getline(file, line);
-			stringstream ss(line);
-			ss >> temp >> temp >> name >> score;
-			scores[i].name = name;
-			scores[i].score = score;
-		}
-	}
 }
 
 void showScores(vector<Score> &scores)
 {
 	for (int i = 0; i < 10; i++)
-		cout << scores[i].name << " - " << scores[i].score << endl;
+		cout << i + 1 << " - " << scores[i].name << " - " << scores[i].score << endl;
 }
 
 void updateScore(vector<Score> &scores, unsigned int playerScore, string playerName) //nao funciona a 100%
 {
-	int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < scores.size(); i++)
+	for (i; i < scores.size(); i++)
 	{
 		if (playerScore < scores[i].score)
 			break;
 	}
 
-	for (int j = 0; j < scores.size() - 1 - i; j++)
+	for (unsigned int j = i; j < scores.size() - 2; j++)
 	{
-		scores[scores.size() - j - 1].score = scores[scores.size() - j - 2].score;
-		scores[scores.size() - j - 1].name = scores[scores.size() - j - 2].name;
+		if (i == 0)
+		{
+			cout << "Yet to do \n";
+			
+			break;
+		}
+		else if (j == i)
+		{
+			scores[j + 2] = scores[j + 1];
+			scores[j + 1] = scores[j];
+		}
+		
+		else
+			scores[j + 2] = scores[j + 1];
 	}
-
 	scores[i].name = playerName;
 	scores[i].score = playerScore;
 
+	/*for (int j = 0; j < scores.size() - 1 - i; j++)
+	{
+		scores[scores.size() - j - 1].score = scores[scores.size() - j - 2].score;
+		scores[scores.size() - j - 1].name = scores[scores.size() - j - 2].name;
+	}*/
+
+	//scores[i].name = playerName;
+	//scores[i].score = playerScore;
 	fstream file("Highscores.txt");
 	string line, temp, name;
 
 	file << "Battleship Highscores" << endl;
-	for (int i = 0; i < 10; i++)
+	for (unsigned int j = 0; j < 10; j++)
 	{
-		file << i + 1 << "  -  " << scores[i].name << " - " << scores[i].score << endl;	
+		file << j + 1 << "  -  " << scores[j].name << " - " << scores[j].score << endl;	
 	}
 	file.close();
 }
 
-int main()
+void game()
 {
 	srand((unsigned)time(NULL));
 	
@@ -173,9 +195,52 @@ int main()
 	}
 		*/
 
-	readScores(scores);
 	updateScore(scores, 11, "Miquelina");
 	showScores(scores);
 
-	return 0;
+}
+
+void gotoxy1(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int main()
+{
+	int option;
+	bool validoption = true;
+
+	vector<Score> scores(10);
+	readScores(scores);
+
+
+	gotoxy1(35, 0);
+	cout << "BATTLESHIP \n\n";
+	gotoxy1(0, 2);
+	cout << "1 - Play Game" << endl;
+	cout << "2 - View Highscores" << endl;
+	cout << "3 - Create a new table \n\n";
+	do
+	{
+		cout << "Please select an option: ";
+		cin >> option;
+		if (cin.fail() || cin.eof() || option > 3 || option < 1)
+		{
+			validoption = false;
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		else validoption = true;
+	} while (!validoption);
+	if (option == 1)
+		game();
+	else if (option == 2)
+	{
+		updateScore(scores, 35, "Jorge");
+		showScores(scores);
+	}
+	
 }
