10bd0faac72f73a007f0d52811225b0c90ea3027
Fábio Caramelo <fabiocaramelo18@gmail.com>
2015-05-25 20:08:39 +0100

    comentarios board adicionados

--- Proj 2/BOARD.cpp
+++ Proj 2/BOARD.cpp
@@ -22,21 +22,21 @@ Board::Board(const string &filename) // loads board from file 'filename'
 
 	ifstream fich(filename);
 	
-	fich >> numLines >> temp >> numColumns;
+	fich >> numLines >> temp >> numColumns;  // le do ficheiro as dimensoes do tabuleiro
 
-	while (!fich.eof())
+	while (!fich.eof())  // ciclo que le o ficheiro e coloca no vector de ships um navio com a informacao lida 
 	{
 		char symbol, orientation;
 		Position<char> position;  
 		unsigned int size, color; 
 		
-		fich >> symbol >> size >> position.lin >> position.col >> orientation >> color;
+		fich >> symbol >> size >> position.lin >> position.col >> orientation >> color; 
 
 		ships.push_back(Ship(symbol, position, orientation, size, color));
 	}
 	fich.close();
 	
-	for (int i = 0; i < numLines; i++)
+	for (int i = 0; i < numLines; i++) // cria o board (vector bidimensional) com as dimensoes lidas
 	{
 		vector<int> temp (numColumns, -1);
 		board.push_back(temp);
@@ -51,9 +51,9 @@ bool Board::putShip(const Ship &s, unsigned int num) // adds ship to the board, 
 	bool free = true;
 	if (s.getOrientation() == 'H') // orientacao horizontal
 	{
-		if (column + s.getSize() > numColumns || line < 0 || column < 0 || line > numLines)
+		if (column + s.getSize() > numColumns || line < 0 || column < 0 || line > numLines) // verifica se o navio esta fora do tabuleiro
 			return false;
-		for (int j = 0; j < s.getSize(); j++) // preencher o numero de posicoes correspondentes ao tamanho do navio
+		for (int j = 0; j < s.getSize(); j++) // verifica o numero de posicoes correspondentes ao tamanho do navio
 		{
 			if (board[line][column + j] != -1 && board[line][column + j] != num)
 			{
@@ -64,9 +64,9 @@ bool Board::putShip(const Ship &s, unsigned int num) // adds ship to the board, 
 	}
 	else                           // orientacao vertical
 	{
-		if (line + s.getSize() > numLines || line < 0 || column < 0 || column > numColumns)
+		if (line + s.getSize() > numLines || line < 0 || column < 0 || column > numColumns) // verifica se o navio esta fora do tabuleiro
 			return false;
-		for (int j = 0; j < s.getSize(); j++) // preencher o numero de posicoes correspondentes ao tamanho do navio
+		for (int j = 0; j < s.getSize(); j++) // verifica o numero de posicoes correspondentes ao tamanho do navio
 		{
 			if (board[line + j][column] != -1 && board[line][column + j] != num)
 			{
@@ -97,35 +97,35 @@ bool Board::putShip(const Ship &s, unsigned int num) // adds ship to the board, 
 
 void Board::putShips() 
 {
-	for (unsigned int i = 0; i < ships.size(); i++)
+	for (unsigned int i = 0; i < ships.size(); i++) // vai colocar todos os navios presentes no vector no tabuleiro
 		putShip(ships[i], i);
 }
 
 void Board::moveShips() // tries to randmonly move all the ships of the fleet
 {
 	int count = 0;
-	for (unsigned int i = 0; i < ships.size(); i++)
+	for (unsigned int i = 0; i < ships.size(); i++) // movimenta um navio de cada vez
 	{
 		if (!ships[i].isDestroyed())
 		{
-			Ship temp = ships[i];
-			bool move = ships[i].moveRand(0, 0, numLines - 1, numColumns - 1);
-			if (move || ships[i].getOrientation() != temp.getOrientation())
+			Ship temp = ships[i]; // guarda a informacao do navio
+			bool move = ships[i].moveRand(0, 0, numLines - 1, numColumns - 1); // verifica se o navio se movimentou
+			if (move || ships[i].getOrientation() != temp.getOrientation()) // executa se o navio se movimentou ou rodou
 			{
-				deleteShip(temp);
-				while (count <= 20)
+				deleteShip(temp); // apaga o navio original do tabuleiro
+				while (count <= 20) // o programa tentara 20 vezes colocar o navio nas novas posicoes
 				{
 					if (putShip(ships[i], i))
 						break;
-					else if (count == 20)
+					else if (count == 20) // caso no final de 20 iteracoes nao tiver colocado o navio, volta a posicao original
 					{
 						ships[i] = temp;
 						putShip(temp, i);
 					}
 					else
 					{
-						ships[i] = temp;
-						ships[i].moveRand(0, 0, numLines - 1, numColumns - 1);
+						ships[i] = temp; // coloca no vector o navio original
+						ships[i].moveRand(0, 0, numLines - 1, numColumns - 1); // movimento de rotacao do navio novamente
 					}
 					count++;
 				}
@@ -144,14 +144,14 @@ void Board::deleteShip(const Ship &s)
 	int column = s.getPosition().col;
 	if (s.getOrientation() == 'H') // orientacao horizontal
 		{
-			for (int j = 0; j < s.getSize(); j++) // preencher o numero de posicoes correspondentes ao tamanho do navio
+			for (int j = 0; j < s.getSize(); j++) // apagar o numero de posicoes correspondentes ao tamanho do navio
 			{
 				board[line][column + j] = -1;
 			}
 		}
 	else                           // orientacao vertical
 	{
-		for (int j = 0; j < s.getSize(); j++) // preencher o numero de posicoes correspondentes ao tamanho do navio
+		for (int j = 0; j < s.getSize(); j++) // apagar o numero de posicoes correspondentes ao tamanho do navio
 		{
 			board[line + j][column] = -1;
 		}
@@ -163,34 +163,34 @@ bool Board::attack(const Bomb &b)
 	bool hit = true;
 	Position<unsigned int> coordenates;
 	int partNumber;
-	coordenates.lin = (int)(b.getTargetPosition().lin - 'A');
-	coordenates.col = (int)(b.getTargetPosition().col - 'a');
+	coordenates.lin = (int)(b.getTargetPosition().lin - 'A'); // conversao das coordenadas para inteiros
+	coordenates.col = (int)(b.getTargetPosition().col - 'a'); // conversao das coordenadas para inteiros
 
 	clrscr();
 	gotoxy(0, 0);
 
-	if (coordenates.lin < 0 || coordenates.col < 0 || coordenates.lin > (unsigned int) numLines - 1 || coordenates.col > (unsigned int) numColumns - 1)
+	if (coordenates.lin < 0 || coordenates.col < 0 || coordenates.lin > (unsigned int) numLines - 1 || coordenates.col > (unsigned int) numColumns - 1) // verifica se a bomba caiu fora do tabuleiro
 		hit = false;
-	else if (board[coordenates.lin][coordenates.col] == -1 )
+	else if (board[coordenates.lin][coordenates.col] == -1 ) //caso a bomba tenha acertado no "mar" nao atinge um navio
 		hit = false;
 
 	if (hit)
 	{
 		if (ships[board[coordenates.lin][coordenates.col]].getOrientation() == 'H')
-			partNumber = coordenates.col - ships[board[coordenates.lin][coordenates.col]].getPosition().col;
+			partNumber = coordenates.col - ships[board[coordenates.lin][coordenates.col]].getPosition().col; // calcula o indice do navio que foi atingido
 		else if (ships[board[coordenates.lin][coordenates.col]].getOrientation() == 'V')
-			partNumber = coordenates.lin - ships[board[coordenates.lin][coordenates.col]].getPosition().lin;
+			partNumber = coordenates.lin - ships[board[coordenates.lin][coordenates.col]].getPosition().lin; // calcula o indice do navio que foi atingido
 
-		char h = ships[board[coordenates.lin][coordenates.col]].getStatus()[partNumber];
-		if (islower(h))
+		char h = ships[board[coordenates.lin][coordenates.col]].getStatus()[partNumber]; // guarda o caracter do indice da posicao atingida
+		if (islower(h))  // se a posicao ja tiver sido atingida avisa
 		{
 			cout << "Position has already been hit! \n";
 		}
 
-		ships[board[coordenates.lin][coordenates.col]].attack(partNumber);
+		ships[board[coordenates.lin][coordenates.col]].attack(partNumber); // vai mudar para minuscula a posicao atingida
 		setcolor(10, 0);
 		cout << "\nHit! \n";
-		if (ships[board[coordenates.lin][coordenates.col]].isDestroyed())
+		if (ships[board[coordenates.lin][coordenates.col]].isDestroyed()) // avisa quando o navio for destruido
 		{
 			setcolor(7, 0);
 			cout << "Ship "; 
@@ -198,7 +198,7 @@ bool Board::attack(const Bomb &b)
 			cout << ships[board[coordenates.lin][coordenates.col]].getSymbol();
 			setcolor(7, 0);
 			cout << " has been destroyed!";
-			deleteShip(ships[board[coordenates.lin][coordenates.col]]);
+			deleteShip(ships[board[coordenates.lin][coordenates.col]]); // elimina o navio do board
 		}
 		setcolor(7, 0);
 	}
@@ -233,18 +233,18 @@ void Board::display() const // displays the colored board during the game
 				setcolor(7, 0);
 				cout << (char)('A' + i); // Identificação das linhas
 			}
-			else if (board[i][k] == -1)
+			else if (board[i][k] == -1) // se no vector estiver -1 imprime um ponto (mar)
 			{
 				setcolor(9, 15);
 				std::cout << setw(2) << ".";
 			}
 			else
 			{
-				setcolor(ships[board[i][k]].getColor(), 15);
+				setcolor(ships[board[i][k]].getColor(), 15); // selecciona a cor a imprimir correspondente 
 				if (ships[board[i][k]].getOrientation() == 'H')
-					std::cout << setw(2) << ships[board[i][k]].getStatus()[k - ships[board[i][k]].getPosition().col];
+					std::cout << setw(2) << ships[board[i][k]].getStatus()[k - ships[board[i][k]].getPosition().col]; //imprime a letra correspondente a posicao do navio
 				else if (ships[board[i][k]].getOrientation() == 'V')
-					std::cout << setw(2) << ships[board[i][k]].getStatus()[i - ships[board[i][k]].getPosition().lin]; 
+					std::cout << setw(2) << ships[board[i][k]].getStatus()[i - ships[board[i][k]].getPosition().lin]; //imprime a letra correspondente a posicao do navio
 			}
 		}
 		std::cout << endl;
@@ -254,7 +254,7 @@ void Board::display() const // displays the colored board during the game
 
 void Board::show() const// shows the attributes of the board (for debugging)
 {
-	for (int i = 0; i < numLines; i++)
+	for (int i = 0; i < numLines; i++) 
 	{
 		for (int k = 0; k < numColumns; k++)
 		{
@@ -273,7 +273,7 @@ vector<Ship> Board::getShips() const
 bool Board::areDestroyed() const
 {
 	bool destroyed = true;
-	for (unsigned int i = 0; i < ships.size(); i++)
+	for (unsigned int i = 0; i < ships.size(); i++) // verifica se todos os navios estao destruidos
 	{
 		if (ships[i].isDestroyed() == false)
 			destroyed = false;
